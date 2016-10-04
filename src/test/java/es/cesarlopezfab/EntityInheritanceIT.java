7d2bbdc5814c88e786bae40a37434c42f5537b07
cesarlopezfa <cesarlopezfa@gmail.com>
2016-10-04 20:31:41 +0200

    Rename test profile to memoryDB

--- src/test/java/es/cesarlopezfab/EntityInheritanceIT.java
+++ src/test/java/es/cesarlopezfab/EntityInheritanceIT.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("memoryDB")
 public class EntityInheritanceIT {
 
 	@Autowired
